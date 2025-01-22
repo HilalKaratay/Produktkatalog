376fa5562c4484c63d874df704901159e0a85f63
it.praktikant1 <it.praktikant1@PRAKTI1-LPT>
2025-01-22 09:05:12 +0100

    Projektdateien hinzufügen.

---
+++ Produktkatalog/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produktkatalog.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private string _logoPath;
+
+    
+
+     public string LogoPath
+        {
+            get { return _logoPath; } 
+            set { _logoPath = value; OnPropertyChanged(nameof(LogoPath));}
+        }
+
+        public MainWindowViewModel()
+        {
+            LogoPath = @"..\Resource\Dial.png";
+           
+        }
+    }
+}
---
+++ Produktkatalog/ViewModel/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produktkatalog.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged //Das ist das Interface,welches die Eingaben der Benutzer erkennt.
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
